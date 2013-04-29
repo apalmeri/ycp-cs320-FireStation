74f3bb51b1a83a5152e16838f60968ec369765f1
Alana Palmerini <apalmeri@ycp.edu>
2013-04-29 11:14:50 -0400

    nothing relevant yet

---
+++ ycp-cs320-project-tests/src/edu/ycp/cs320/controllers/GetContactInfoController.java
@@ -0,0 +1,29 @@
+package edu.ycp.cs320.controllers;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import edu.ycp.CS320.shared.ContactInfo;
+import edu.ycp.CS320.shared.IDatabase;
+
+
+public class GetContactInfoController implements Serializable {
+	
+	
+	
+	/*@Override
+	public ArrayList<ContactInfo> loadContactInfo() {
+		GetContactInfoController contactInfoController = new GetContactInfoController();
+		
+		IDatabase db = DatabaseSingleton.instance();
+		
+		ArrayList<ContactInfo> result = contactInfoController.getInfo(db);
+		if (result == null) {
+			result = new ArrayList<ContactInfo>();
+		}
+		return result;
+			
+	}
+	*/
+	
+}
